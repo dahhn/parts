f0af0d876b59f44a146092da142b0a4abc8a0a42
Daniel Hahn <DAHHN@vestas.com>
2022-04-29 12:56:20 -0500

    copied search function into file

--- scrub.R
+++ scrub.R
@@ -2,7 +2,11 @@ scrub <- function(my_pdf){
   require(tidyverse)
   require(pdftools)
   
-  
-  
-  
+  # handy search function that searches a list for specfic patterns and removes chr(0) elements
+  search <- function(list, reg_express){
+    #search feature
+    a <- sapply(list, grep, pattern=reg_express, value=TRUE)
+    #remove chr(0) elements
+    compact(a)
+  }
 }
