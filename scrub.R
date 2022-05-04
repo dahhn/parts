e26ba3d88e26022ec024d209987e9f803fae19f3
Daniel Hahn <DAHHN@vestas.com>
2022-05-04 17:06:31 -0500

    separated search function. renamed file to scrub.

--- scrub.R
+++ scrub.R
@@ -1,13 +1,14 @@
-scrub <- function(my_pdf){
-  require(tidyverse)
-  require(pdftools)
-  
-  # handy search function that searches a list for specfic patterns and removes chr(0) elements
-  search <- function(list, reg_express){
-    #search feature
-    a <- sapply(list, grep, pattern=reg_express, value=TRUE)
-    #remove chr(0) elements
-    compact(a)
-  }
+scrub <- function(item){
+  a <- gsub("─","-",item) #remove box line character and replace with dash
+  b <- gsub('[\",)]',"",a) #remove quotes, commas
+  c <- gsub('(\\\\n)', " ", b) #remove \\n new line return
+  d <- gsub('(c[[:punct:]])', "", c) #remove any c( characters
+  e <- gsub(" ", "",d) # remove whitespace
+  cleaned_data <<- e
+  cleaned_data
 }
+
+item_parse(f)
+
+
 
