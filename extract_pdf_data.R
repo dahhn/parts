86084337232e751f2576d1fe4f14563e41c655dc
Daniel Hahn <DAHHN@vestas.com>
2022-05-04 17:05:22 -0500

    renamed file. created a function. needs testing

---
+++ extract_pdf_data.R
@@ -0,0 +1,28 @@
+extract_pdf_data <- function(){
+  #pull data from the pdf and save as vector
+  v120hub<-pdf_text(paste0(getwd(),"/V120hub.pdf"))
+  # random page saved for testing.
+  a <- v120hub[49]
+  #split up list into individual spaces and words
+  b<- strsplit(x=a, split=" ")
+  # regex extracts from y any [digit, word, or period, dash (-), + or line like character {2500}] as a whole word and removes white space
+  #c <- str_extract_all(b, "[\\d|\\w|\\.|\\-|\\n|\\+|\\x{2500}]+")  --- will remove later. Saving for now
+  c <- str_extract_all(b, "(\\S)+")
+  #changing to be able to more easily access elements in c
+  d<-as.list(c[[1]])
+  #search for all words "\\w" that are not and "n" [^n] multiple times "+"
+  e<-search(d,"([^n])\\w+")
+  # search for any dash like characters, word characters 
+  f <- search(e, "\\x{2500}|\\w")
+  extracted_data <<- f
+}
+#### Below is only for testing. Will be incorperated into another function in the future.
+
+# looks for any number that is between 6 and 9 digits
+search(f, "\\d{6,9}")
+# search for any non-digit character
+search(f, "\\D")
+# search for the column names
+search(f, "Pos|Qty|Material|Description|Codes|Remarks")
+
+
