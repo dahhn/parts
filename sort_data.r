736cf5d746fe13552f9bf49d4b2b051700b793db
Daniel Hahn <DAHHN@vestas.com>
2022-05-10 05:57:18 -0500

    developed aspects of function to search and assign designation, material, position, and quantity vectors. mapped vectors as columns in a tibble. still under development but big steps today.

--- sort_data.r
+++ sort_data.r
@@ -1,5 +1,6 @@
 sort_data <- function(){
-  split_index <- match("Pos",(cleaned_data))
+require(tidyverse)
+    split_index <- match("Pos",(cleaned_data))
   metadata_vec <- cleaned_data[1:split_index-1]
   data_vec <- cleaned_data[split_index+7:length(cleaned_data)]
   data_vec <- na.omit(data_vec)
@@ -12,59 +13,41 @@ sort_data <- function(){
   description <-c()
   codes <- c()
   remarks <-c()
-  
-  designation_1 <- as.vector(grep("(-\\d{3}-\\d{2})",data_vec, value = FALSE))
-  designation_2 <- as.vector(grep("(-\\d{3}-.._.)",data_vec, value = FALSE))
+  # this regex is looking for "-nnn-nn" where n is any number
+  designation_indexes_type1 <- as.vector(grep("(-\\d{3}-\\d{2})",data_vec, value = FALSE))
+  # this regex is looking for "-nnn-aa_a" where a is any character
+  designation_indexes_type2 <- as.vector(grep("(-\\d{3}-.._.)",data_vec, value = FALSE))
   designation <-sort(c(designation_1, designation_2))
+  # this regex is looking for any number that is between 6 and 10 digits long
+  material_indexes <- as.vector(grep("(\\d{6,10})",data_vec, value = FALSE))
+  quantity_indexes <- material_indexes - 1
+  position_indexes <- material_indexes - 2
   
-  material_no <- as.vector(grep("(\\d{6,10})",data_vec, value = FALSE))
-  
-  #loop for cleaning and combining designations
-  for(i in designation){
+  #loop for cleaning and combining designations in the codes vector
+for(i in designation){
     if(i %in% designation_2 == TRUE){
       codes <-c(codes, data_vec[i])
     } else {
       codes <- c(codes,paste0(data_vec[i],data_vec[i+1]))}
   }
-  
-  count <- 0# this is a general counter to help the sort loop below
-  numbers<-as.numeric(grep("\\d",i, value = TRUE))  
-  #loop to find numbers
- for(i in material_no){
-   material_no <- c(material_no, data_vec[i])
- }
 
-  
-  position
-  quantity
-  material
-  codes
+  #loop for setting material numbers 
+for(i in material_indexes){
+   material <- c(material, as.numeric(data_vec[i]))
+}
+#loop for setting quantity numbers  
+for(i in quantity_indexes){
+  quantity <- c(quantity, as.numeric(data_vec[i]))
+}
+#loop for setting position numbers 
+for(i in position_indexes){
+  position <- c(position, as.numeric(data_vec[i]))
+}
+df <- tibble(material=material,quantity=quantity,codes=codes, position=position)
   
 }
 
 sort_data()
 
 
-
-
-for(i in data_vec){
-  if(count!= 3){
-    if(is.na(numbers)== FALSE){
-      if(count == 0 ){
-        position <- c(position, as.numeric(i))
-        count <- count+1 
-        position
-      }
-      else if(count == 1 ){
-        quantity <- c(quantity, as.numeric(i))
-        count <- count+1
-        quantity
-      }
-      else if(count == 2 ){
-        material <- c(material, as.numeric(i))
-        count <- count+1
-        material
-      }
-    }
-  }
-}
+df
