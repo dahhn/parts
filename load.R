989cadae388656457393f143026a2be41e65d413
Daniel Hahn <DAHHN@vestas.com>
2022-05-26 18:59:43 -0500

    Function to load all packages. Changed name

--- load.R
+++ load.R
@@ -1,10 +1,15 @@
 #install.packages("tidyverse")
 #install.packages("pdftools")
-load_files <- function(){
+inst_pack <- function(){
   library(tidyverse)
   library(pdftools)
-  library(assertive) 
+  library(assertive)
+  #library(assertive)
+  #library(data.table)
 }
+inst_pack()
+v120hub<-pdf_text(paste0(getwd(),"/V120hub.pdf"))
+scrub(v120hub)
 
 
 
